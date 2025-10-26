678fcd0e6faab2387275e9b12abe8176bfbab5f3
Kuba Hernet <jakub.hernet@gmail.com>
2025-10-26 07:13:00 +0100

    fetch top 10 offers details

--- just_join_it/just_join_it_client.py
+++ just_join_it/just_join_it_client.py
@@ -1,17 +1,19 @@
 import requests
 
-base_url = "https://justjoin.it/api/base/v2/user-panel/offers/by-cursor"
+base_url = "https://justjoin.it/api"
+get_offers_endpoint = f"{base_url}/base/v2/user-panel/offers/by-cursor"
+get_offer_details_endpoint = f"{base_url}/justjoinit/job-advertisements/"
 
 params = {
     "orderBy": "DESC",
     "sortBy": "salary",
     "categories[]": [5, 25],
-    "itemsCount": 5,
+    "itemsCount": 10,
     "remoteWorkOptions[]": "remote"
 }
 
 def get_offers():
-    response = requests.get(base_url, params=params)
+    response = requests.get(get_offers_endpoint, params=params)
 
     print(f"Requested URL: {response.url}")
 
@@ -23,3 +25,15 @@ def get_offers():
     else:
         print(f"Failed to retrieve data: {response.status_code}")
         return []
+    
+def get_offer_details(offer_id):
+    url = f"{get_offer_details_endpoint}{offer_id}"
+    response = requests.get(url)
+
+    print(f"Requested URL: {response.url}")
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to retrieve offer details for ID {offer_id}: {response.status_code}")
+        return None
--- main.py
+++ main.py
@@ -6,11 +6,19 @@ from utils.collection_utils import remove_duplicates
 offers = just_join_it_client.get_offers()
 
 if len(offers) > 0:
-    unique_offers =   remove_duplicates(offers, lambda x: x.get("guid"))
+    offers = remove_duplicates(offers, lambda x: x.get("guid"))
 
-    with open("offers.json", "w") as file:
-        json.dump(unique_offers, file, indent=4)
+    with open("offers/just_join_it/offers.json", "w") as file:
+        json.dump(offers, file, indent=4)
 
     print("Data saved to offers.json")
+
+    top_ten_offers = offers[:10]
+    for offer in top_ten_offers:
+        offer_id = offer.get("guid")
+        details = just_join_it_client.get_offer_details(offer_id)
+        if details:
+            with open(f"offers/just_join_it/items/{offer.get("slug")}.json", "w") as file:
+                json.dump(details, file, indent=4)
 else:
     print("Failed to retrieve data or no offers found.")
