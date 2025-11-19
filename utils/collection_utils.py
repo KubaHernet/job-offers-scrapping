99c0eadfb380a2aa18238e5cde06f8ee4e5bcb3b
Kuba Hernet <jakub.hernet@gmail.com>
2025-11-19 21:55:11 +0100

    add type hints in collection_utils

--- utils/collection_utils.py
+++ utils/collection_utils.py
@@ -1,9 +1,19 @@
-def remove_duplicates(items, selector):
-    seen = set()
-    unique_items = []
+from typing import TypeVar, Callable, Optional
+
+TListItem = TypeVar('TListItem')
+TItemSelector = TypeVar('TItemSelector')
+
+def remove_duplicates(
+        items: list[TListItem],
+        selector: Callable[[TListItem], Optional[TItemSelector]]
+) -> list[TListItem]:
+    seen: set[TItemSelector] = set()
+    unique_items: list[TListItem] = []
+    
     for item in items:
         key = selector(item)
         if key and key not in seen:
             unique_items.append(item)
             seen.add(key)
+
     return unique_items
